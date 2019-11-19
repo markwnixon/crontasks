25cd150fc23ff95cf14e3b0fbef819fb433d32a3
Mark Nixon <markwnixon@gmail.com>
2019-11-19 05:04:41 -0500

    run

--- AAA_shipreport.py
+++ AAA_shipreport.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as soup
 import datetime
 import time
+import operator
 
 from email_reports import emailshipreport
 
@@ -13,33 +14,65 @@ from CCC_system_setup import mycompany
 co = mycompany()
 if co == 'FELA':
     from CCC_FELA_remote_db_connect import tunnel, db
-    from CCC_FELA_models import Bookings, OverSeas
+    from CCC_FELA_models import Bookings, OverSeas, FELBills
 elif co == 'OSLM':
     from CCC_OSLM_remote_db_connect import tunnel, db
     from CCC_OSLM_models import Bookings, OverSeas
 
 today=datetime.date.today()
-dragday=today-datetime.timedelta(days=20)
-advday=today+datetime.timedelta(days=20)
-bookings=Bookings.query.filter((Bookings.SailDate<advday) & (Bookings.EstArr>dragday)).order_by(Bookings.EstArr).all()
-    
-if 1==1:
-    print('Running Ship Script',flush=True)
-    with Display():
-        browser = webdriver.Firefox()
-        tabdata=[]
+fromday=today-datetime.timedelta(days=60)
+futureday=today+datetime.timedelta(days=10)
+#bookings=Bookings.query.filter((Bookings.SailDate<advday) & (Bookings.EstArr>dragday)).order_by(Bookings.EstArr).all()
+odata=OverSeas.query.filter((OverSeas.PuDate<futureday) & (OverSeas.PuDate>fromday)).all()
 
-        for book in bookings:
+def convert_date_dt(indate,inform):
+    adate = indate[0:11]
+    adate = adate.strip()
+    adatedt = datetime.datetime.strptime(adate, inform)
+    return adatedt.date()
+
+def convert_date(indate,inform):
+    try:
+        adate = indate[0:11]
+        adate = adate.strip()
+        adatedt = datetime.datetime.strptime(adate, inform)
+        adatedt = adatedt.date()
+        delta = adatedt - today
+        daysaway = delta.days
+        date1str = adatedt.strftime('%Y-%m-%d')
+    except:
+        daysaway = 0
+        date1str = date2str
+
+    return date1str, daysaway
+
+
+with Display():
+#if 1 == 1:
+
+    tabdata=[]
+
+    for odat in odata:
+        bk = odat.Booking
+        container = odat.Container
+        container = container[0:11]  # in case we added letters to designate street turn
+        container = container.strip()
+
+        book = Bookings.query.filter(Bookings.Booking == bk).first()
+        if book is not None:
             adate='Not Found'
             seastatus='Unknown'
             portstatus='Unknown'
             daysaway=0
-            odat=OverSeas.query.filter(OverSeas.Booking==book.Booking).first()
-            if odat is not None:
-                container=odat.Container
-                container=container[0:11] # in case we added letters to designate street turn
-                container=container.strip()
-                time.sleep(2)
+            date2dt = book.EstArr
+            date2str = date2dt.strftime('%Y-%m-%d')
+            bk = book.Booking
+            shipline = book.Line
+
+            time.sleep(2)
+            if shipline == 'Maersk':
+                browser = webdriver.Firefox()
+                browser.set_window_size(1920, 1080)
                 url='https://www.maersk.com/tracking/#tracking/'+container
                 browser.get(url)
                 time.sleep(1)
@@ -63,22 +96,9 @@ if 1==1:
                         adate2=allspans[j+1]
                         portstatus='Still at Port'
                         seastatus='Still at Port'
-                        
-                    try:
-                        adate=adate[0:11]
-                        adate=adate.strip()
-                        adatedt=datetime.datetime.strptime(adate, '%d %b %Y')
-                        adatedt=adatedt.date()
-                        delta=adatedt-today
-                        daysaway=delta.days
-                        date1str=adatedt.strftime('%Y-%m-%d')
-                    except:
-                        daysaway=0
-                        date1str='None'
-                        
-                    date2dt=book.EstArr
-                    date2str=date2dt.strftime('%Y-%m-%d')
-                    
+
+                    date1str, daysaway = convert_date(adate,'%d %b %Y')
+
                     if portstatus=='Has Arrived':
                         allstatus='Arrived'
                     elif portstatus=='Left Port' and seastatus=='At Sea':
@@ -87,22 +107,79 @@ if 1==1:
                         allstatus='Not Left Yet'
                     else:
                         allstatus = 'Unknown'
-                        
-                tabdata.append([str(daysaway),allstatus,date1str,date2str,book.Booking,container,book.Vessel,odat.BillTo,odat.Exporter])
-    #arrivals=page_soup.findAll('span',{'class':'pseudo-header--data-key'})
-    #for arrive in arrivals:
-        #print(arrive.text)
-    
-    
-    #print(browser.title) #this should print "Google"
 
-    #finally:
-    browser.quit()
-    print('Days','Status','ArriveUpdate','PlannedArrive','Booking','Container','Vessel','BillTo','Exporter')
-    for tab in tabdata:
-        print(tab[:9])
-    emailshipreport(tabdata)
-    
+                browser.quit()
+
+            elif shipline == 'Hoegh':
+                browser = webdriver.Firefox()
+                browser.set_window_size(1920, 1080)
+                url='https://www.hoeghautoliners.com/my-cargo'
+                browser.get(url)
+                time.sleep(2)
+
+                booksend = browser.find_element_by_xpath('//*[@id="booking-or-bl"]')
+                booksend.send_keys(bk)
+                time.sleep(1)
+
+                browser.find_element_by_xpath('//*[@id="btn-send"]').click()
+
+                time.sleep(1)
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[1]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                selectElem.click()
+
+                time.sleep(1)
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[2]/table/tbody/tr[2]/td[2]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                voyage = selectElem.text
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[2]/table/tbody/tr[3]/td[2]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                lport = selectElem.text
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[2]/table/tbody/tr[5]/td[2]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                depdate = selectElem.text
+                depdate_dt = convert_date_dt(depdate, '%d.%m.%Y')
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[2]/table/tbody/tr[6]/td[2]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                disport = selectElem.text
+
+                contentstr = f'//*[@id="my-cargo-result"]/ul/li[1]/div[2]/table/tbody/tr[7]/td[2]'
+                selectElem = browser.find_element_by_xpath(contentstr)
+                arrdate = selectElem.text
+                date1str, daysaway = convert_date(arrdate, '%d.%m.%Y')
+                arrdate_dt = convert_date_dt(arrdate, '%d.%m.%Y')
+                allstatus = 'Not Left Yet'
+                if today > depdate_dt:
+                    allstatus = 'At Sea'
+                if today >= arrdate_dt:
+                    allstatus = 'Arrived'
+
+                print(voyage, lport, depdate, disport, arrdate)
+
+                browser.quit()
+
+            # Get payment status:
+            bdat = FELBills.query.filter( (FELBills.Memo.contains(bk)) | (FELBills.Description.contains(bk)) ).first()
+            if bdat is not None:
+                pay = 'Paid'
+            else:
+                pay = 'Pmt Due'
+
+            if (shipline == 'Hoegh' or shipline == 'Maersk') and daysaway > -5:
+                tabdata.append([str(daysaway),allstatus,date1str,pay,date2str,bk,container,book.Vessel,odat.BillTo,odat.Exporter])
+
+
+
+    print('Days','Status','ArriveUpdate','Release Pmt', 'PlannedArrive','Booking','Container','Vessel','BillTo','Exporter')
+    newtabdata = sorted(tabdata, key = operator.itemgetter(2))
+    for tab in newtabdata:
+        print(tab[:10])
+    #emailshipreport(tabdata)
+
     
 tunnel.stop()
-        #print([x for x in tab[:9]])
---
+++ find_duplicates.py
@@ -0,0 +1,71 @@
+import requests
+import json
+import datetime
+import calendar
+from math import sqrt
+from cronfuncs import dropupdate, d1s, d2s
+from datetime import timedelta
+
+
+daybackfrom = 50
+daybackto = 0
+printif = 0
+# (daybackto=0 is today; from 1 to 0 is yesterday and today)
+
+
+today = datetime.datetime.today()
+#today = today.date()
+print(today) if printif == 1 else 1
+
+from CCC_system_setup import co
+
+if co == 'FELA':
+    from CCC_FELA_remote_db_connect import tunnel, db
+    from CCC_FELA_models import Orders, Interchange
+
+elif co == 'OSLM':
+    from CCC_OSLM_remote_db_connect import tunnel, db
+    from CCC_OSLM_models import Orders, Interchange
+
+odata = Orders.query.filter(Orders.Shipper == 'Global Business Link').order_by(Orders.Date).all()
+
+if 1 == 2:
+    num_dups = 0
+    for odat in odata:
+        bk = odat.Booking
+        cn = odat.Container
+        if cn == 'TBD':
+            cn = 'NOM'
+        tid = odat.id
+        bdat = Orders.query.filter( ((Orders.Booking == bk) | (Orders.Container == cn)) & (Orders.id != tid) ).first()
+        if bdat is not None:
+            num_dups = num_dups + 1
+            print(f'Have duplicate with {bk} and/or {cn}')
+    print(f'Found {num_dups} duplicates')
+if 1 == 2:
+    # Now make report of global jobs that have been invoiced.
+    for jx, odat in enumerate(odata):
+        status = odat.Status
+        ck = int(status[1])
+        if ck >= 1:
+            dt = odat.Date
+            bk = odat.Booking
+            cn = odat.Container
+            print(f'{jx+1} {dt} {bk} | {cn} already invoiced')
+
+# Now make report of global jobs that have been invoiced.
+jx = 0
+for odat in odata:
+    status = odat.Status
+    ck1 = int(status[0])
+    ck2 = int(status[1])
+    if ck1 == 2 and ck2 ==0:
+        jx = jx+1
+        dt = odat.Date
+        dt2 = odat.Date2
+        bk = odat.Booking
+        cn = odat.Container
+        print(f'{jx} Out:{dt} In:{dt2} Bk:{bk} | Cn:{cn} Not Invoiced')
+
+
+tunnel.stop()
