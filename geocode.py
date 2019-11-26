43437de0edebf680b61d653a41ca03f2d964dfdc
Mark Nixon <markwnixon@gmail.com>
2019-11-25 22:04:25 -0500

    run

--- geocode.py
+++ geocode.py
@@ -8,6 +8,37 @@ print(apikeys)
 API_KEY_GEO = apikeys['gkey']
 API_KEY_DIS = apikeys['dkey']
 
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+def direct_resolver(json):
+    print(json)
+    dirdata = extract_values(json, 'html_instructions')
+    lats = extract_values(json, 'lat')
+    lons = extract_values(json, 'lng')
+    txts = extract_values(json,'text')
+    miles = []
+    for txt in txts:
+        if 'mi' in txt and 'min' not in txt:
+            miles.append(txt)
+    return dirdata, lats, lons, miles
+
 def route_resolver(json):
     print(json)
     final = json['rows']
@@ -46,6 +77,16 @@ def address_resolver(json):
         final['street_number'] = data.get('street_number', None)
     return final
 
+def checkcross(lam,la_last,la,lom,lo_last,lo):
+    lacross = 0
+    locross = 0
+    if (la_last<lam and la>lam) or (la_last>lam and la<lam):
+        lacross = 1
+    if (lo_last<lom and lo>lom) or (lo_last>lom and lo<lom):
+        locross = 1
+    return lacross, locross
+
+
 def get_address_details(address):
     url = 'https://maps.googleapis.com/maps/api/geocode/json?'
     url = url + 'address='+ address.replace(" ","+")
@@ -70,19 +111,29 @@ def get_distance(start,end):
     data = route_resolver(response.json())
     return data
 
+def get_directions(start,end):
+    start = start.replace(" ", "+")
+    end = end.replace(" ", "+")
+    url = f'https://maps.googleapis.com/maps/api/directions/json?origin={start}&destination={end}'
+    url = url + f'&key={API_KEY_DIS}'
+    print(url)
+    response = get(url)
+    dirdata, lats, lons, miles = direct_resolver(response.json())
+    return dirdata, lats, lons, miles
 
-"""
-Provide the address via csv or paste it here 
-"""
-# address_to_search = list(DictReader("path/to/csv/file"))
-address_to_search = ['71 Pilgrim Avenue Chevy Chase, MD 20815']
-data = []
-for i in address_to_search:
-    data.append(get_address_details(i))
-    print(get_address_details(i))
+if 1 == 2:
+    """
+    Provide the address via csv or paste it here 
+    """
+    # address_to_search = list(DictReader("path/to/csv/file"))
+    address_to_search = ['71 Pilgrim Avenue Chevy Chase, MD 20815']
+    data = []
+    for i in address_to_search:
+        data.append(get_address_details(i))
+        print(get_address_details(i))
 
 start = '2600 Broening Highway, Baltimore, MD'
-end = 'Hagerstown, MD'
+end = 'Pittsburgh, PA'
 dist,dura = get_distance(start,end)
 print(dist,dura)
 dist = dist.replace('mi','')
@@ -98,6 +149,65 @@ tothrs = hrs + mins/60.0
 drvcost = tothrs * 27.0
 fuelcost = float(dist)/6.0*2.78
 print(tothrs,drvcost,dist,fuelcost)
+
+
+####################################  Directions Section  ######################################
+dirdata,lats,lons,miles = get_directions(start,end)
+print(len(dirdata), len(lats), len(lats), len(miles))
+npts = len(dirdata)
+nremove = len(lats) - 2*npts
+lats = lats[nremove:]
+lons = lons[nremove:]
+lats = lats[::2]
+lons = lons[::2]
+print(len(lats), len(lons))
+for mi in miles:
+    print(mi)
+for aline in dirdata:
+    print(aline)
+
+
+fm_tollbox =  [39.267757, -76.610192, 39.261248, -76.563158]
+bht_tollbox = [39.259962, -76.566240, 39.239063, -76.603324]
+fsk_tollbox = [39.232770, -76.502453, 39.202279, -76.569906]
+bay_tollbox = [39.026893, -76.417512, 38.964938, -76.290104]
+sus_tollbox = [39.585193, -76.142883, 39.552328, -76.033975]
+new_tollbox = [39.647121, -75.774523, 39.642613, -75.757187] #Newark Delaware Toll Center
+dmb_tollbox = [39.702146, -75.553479, 39.669730, -75.483284]
+tollcodes = ['FM', 'BHT', 'FSK', 'BAY', 'SUS', 'NEW', 'DMB']
+tollboxes = [fm_tollbox, bht_tollbox, fsk_tollbox, bay_tollbox, sus_tollbox, new_tollbox, dmb_tollbox]
+
+for jx,lat in enumerate(lats):
+    stat1 = 'ok'
+    stat2 = 'ok'
+    stat3 = 0
+    stat4 = 0
+    tollcode = 'None'
+    la = float(lat)
+    lo = float(lons[jx])
+    for kx, tollbox in enumerate(tollboxes):
+        lah = max([tollbox[0],tollbox[2]])
+        lal = min([tollbox[0], tollbox[2]])
+        loh = max([tollbox[1],tollbox[3]])
+        lol = min([tollbox[1], tollbox[3]])
+        if la > lal and la < lah:
+            stat1 = 'toll'
+            if lo > lol and lo < loh:
+                stat2 = 'toll'
+                tollcode = tollcodes[kx]
+        if jx > 0:
+            lam = (lah + lal)/2.0
+            lom = (loh + lol)/2.0
+            la_last = float(lats[jx-1])
+            lo_last= float(lons[jx-1])
+            stat3, stat4 = checkcross(lam,la_last,la,lom,lo_last,lo)
+            if stat3 == 1 and stat4 ==1:
+                tollcode = tollcodes[kx]
+    print(lat,lons[jx],stat1, stat2, stat3, stat4, tollcode)
+
+
+
+
 
 
 if 1 == 2:
