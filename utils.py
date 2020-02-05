93ed64dc315044b3e32923121606562c92d5e870
Mark Nixon <markwnixon@gmail.com>
2020-02-04 21:23:54 -0500

    banking updates

---
+++ utils.py
@@ -0,0 +1,23 @@
+def d2s(instr):
+    try:
+        instr = instr.replace('$', '').replace(',', '')
+    except:
+        instr = str(instr)
+    try:
+        infloat = float(instr)
+        outstr = "%0.2f" % infloat
+    except:
+        outstr = instr
+    return outstr
+
+def d1s(instr):
+    try:
+        instr = instr.replace('$', '').replace(',', '')
+    except:
+        instr = str(instr)
+    try:
+        infloat = float(instr)
+        outstr = "%0.1f" % infloat
+    except:
+        outstr = instr
+    return outstr
