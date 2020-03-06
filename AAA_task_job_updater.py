689c1f9910d990d66351262606201d4b79c47069
Mark Nixon <markwnixon@gmail.com>
2020-03-06 07:48:27 -0500

    Merge branch 'master' of github.com:markwnixon/crontasks
    # Conflicts:
    #	AAA_chassis_chain.py

--- AAA_task_job_updater.py
+++ AAA_task_job_updater.py
@@ -37,7 +37,7 @@ for jdat in jdata:
     db.session.commit()
 
 #Sometimes we return container with different booking number, so we need to get the container number to the job whether it is an In or and Out:
-jdata = Orders.query.filter( (~Orders.Status.endswith('3')) & (Orders.Date > lbdate)).all()
+jdata = Orders.query.filter( (Orders.Hstat<3) & (Orders.Date > lbdate)).all()
 for jdat in jdata:
     con = jdat.Container
     if con == 'TBD':
@@ -49,7 +49,7 @@ for jdat in jdata:
                 db.session.commit()
 
 #Make sure there are no doubled up Global Jobs
-jdata = Orders.query.filter( (Orders.Shipper == 'Global Business Link') & (~Orders.Status.endswith('3')) & (Orders.Date > lbdate)).all()
+jdata = Orders.query.filter( (Orders.Shipper == 'Global Business Link') & (Orders.Hstat < 3) & (Orders.Date > lbdate)).all()
 for jdat in jdata:
 
     bk = jdat.Booking
@@ -117,14 +117,14 @@ for idat in idata:
 
 def checkon(con,bk):
     if con == 'TBD' or len(con) < 9:
-        con = 'XXX'
+        retcon = 'XXX'
     else:
         retcon = con
     if len(bk) < 6:
-        bk = 'YYY'
+        retbk = 'YYY'
     else:
         retbk = bk
-    return con, bk
+    return retcon, retbk
 
 
 
@@ -147,10 +147,14 @@ for jdat in jdata:
 
 
 #Make a match of all Jobs with Container Data:
-jdata = Orders.query.filter( (Orders.Hstat < 4) & (Orders.Date > lbdate)).all()
+jdata = Orders.query.filter( (Orders.Hstat < 3) & (Orders.Date > lbdate)).all()
 for jdat in jdata:
     con, bk = checkon(jdat.Container, jdat.Booking)
-    idat = Interchange.query.filter(((Interchange.Container == con) | (Interchange.Release == bk)) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate)).first()
+    idat = Interchange.query.filter(((Interchange.Container == con) & (Interchange.Release == bk)) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate)).first()
+    if idat is None:
+        idat = Interchange.query.filter( (Interchange.Container == con) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate)).first()
+    if idat is None:
+        idat = Interchange.query.filter( (Interchange.Release == bk) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate) ).first()
     if idat is not None:
         #print(f'Match to truck job {idat.Order} and {idat.Container}')
         jdat.Container = idat.Container
@@ -159,11 +163,18 @@ for jdat in jdata:
         idat.Company = jdat.Shipper
         idat.Jo = jdat.Jo
 
-        imat = Interchange.query.filter(((Interchange.Container == con) | (Interchange.Release == bk)) & (Interchange.Type.contains('In')) & (Interchange.Date > lbdate)).first()
+        imat = Interchange.query.filter(((Interchange.Container == con) & (Interchange.Release == bk)) & (Interchange.Type.contains('In')) & (Interchange.Date > lbdate)).first()
+        if imat is None:
+            imat = Interchange.query.filter( (Interchange.Container == con) & (Interchange.Type.contains('In')) & (Interchange.Date > lbdate)).first()
+        if imat is None:
+            imat = Interchange.query.filter(((Interchange.Container == con) & (Interchange.Release == bk)) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate)).first()
+        if imat is None:
+            imat = Interchange.query.filter( (Interchange.Container == con) & (Interchange.Type.contains('Out')) & (Interchange.Date > lbdate)).first()
         if imat is not None:
             jdat.Date2 = imat.Date
             imat.Jo = jdat.Jo
             imat.Company = jdat.Shipper
+            jdat.Chassis = imat.Chassis
         else:
             finishdate = jdat.Date2
             if today > finishdate:
