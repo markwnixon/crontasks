cee63c7da2a9b94a8237afd605bb804e9cad93e1
Mark Nixon <markwnixon@gmail.com>
2020-03-19 09:12:34 -0400

    Merge branch 'master' of github.com:markwnixon/crontasks
    # Conflicts:
    #	AAA_chassis_chain.py

--- AAA_task_gatemonitor.py
+++ AAA_task_gatemonitor.py
@@ -36,7 +36,7 @@ def gatescraper(printif):
     outpath = addpath3('interchange/')
     print('Entering Firefox') if printif == 1 else 1
     yesterday = datetime.strftime(datetime.now() - timedelta(1), '%m/%d/%Y')
-    #todaystr = datetime.strftime(datetime.now() - timedelta(11), '%m/%d/%Y')
+    #todaystr = datetime.strftime(datetime.now() - timedelta(1), '%m/%d/%Y')
     today = datetime.today()
     cutoff = datetime.now() - timedelta(10)
     cutoff = cutoff.date()
